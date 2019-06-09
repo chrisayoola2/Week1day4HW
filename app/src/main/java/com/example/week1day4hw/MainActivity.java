235c7c9274dbd7d0ee641cc90072aa7b70895b6b
Christopher Ayoola <christopher.ayoola@mobileappscompany.us>
2019-06-09 01:45:18 -0400

    fixed all functions

--- app/src/main/java/com/example/week1day4hw/MainActivity.java
+++ app/src/main/java/com/example/week1day4hw/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.week1day4hw;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    int count = 0;  // 0 if Enter button has not been used, 1 if Enter button has been used
+    String operator;
 
+    TextView tvCalculate;
     TextView tvResult;
     Button btn0;
     Button btn9;
@@ -23,15 +28,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnAdd;
     Button btnDecimal;
     Button btnDiv;
-    Button btnEquals;
+    Button btnClear;
     Button btnMin;
     Button btnMul;
+    Button btnEnter;
+
+
+    StringBuilder strBeforeOperator = new StringBuilder();
+    String num1;
+    String num2;
+    String[] equation;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {  // On create method
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         btn0 = findViewById(R.id.btn0);
         btn9 = findViewById(R.id.btn9);
         btn8 = findViewById(R.id.btn8);
@@ -45,9 +59,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = findViewById(R.id.btnAdd);
         btnDecimal = findViewById(R.id.btnDecimal);
         btnDiv = findViewById(R.id.btnDiv);
-        btnEquals = findViewById(R.id.btnEquals);
+        btnClear = findViewById(R.id.btnClear);
         btnMin = findViewById(R.id.btnMin);
         btnMul = findViewById(R.id.btnMul);
+        tvCalculate = findViewById(R.id.tvCalculate);
+        btnEnter = findViewById(R.id.btnEnter);
         tvResult = findViewById(R.id.tvResult);
 
         btn0.setOnClickListener(this);
@@ -65,70 +81,198 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnMul.setOnClickListener(this);
         btnDiv.setOnClickListener(this);
         btnDecimal.setOnClickListener(this);
-        btnEquals.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+        btnEnter.setOnClickListener(this);
+
     }
 
 
-    private  addNumbers(String num1, String num2) {
+    private String addNumbers(String num1, String num2) {  // method that completes addition
+        final double numOne = Double.parseDouble(num1);
+        final double numTwo = Double.parseDouble(num2);
+        return String.valueOf(numOne + numTwo);
     }
 
-    private  subtractNumbers(String num1, String num2) {
+    private String subNumbers(String num1, String num2) {  // method that completes addition
+        final double numOne = Double.parseDouble(num1);
+        final double numTwo = Double.parseDouble(num2);
+        return String.valueOf(numOne - numTwo);
     }
-    private  divideNumbers(String num1, String num2) {
+
+    private String divNumbers(String num1, String num2) {  // method that completes addition
+        final double numOne = Double.parseDouble(num1);
+        final double numTwo = Double.parseDouble(num2);
+        return String.valueOf(numOne / numTwo);
     }
-    private  addNumbers(String num1, String num2) {
+
+    private String mulNumbers(String num1, String num2) {  // method that completes addition
+        final double numOne = Double.parseDouble(num1);
+        final double numTwo = Double.parseDouble(num2);
+        return String.valueOf(numOne * numTwo);
     }
+
+
+
 
 
     @Override
-    public void onClick(View v) {
-       String string = " ";
+    public void onClick(View v) {    // what happens on each click
+
+
         switch (v.getId()) {
             case R.id.btn0:
-                string += 0;
+                strBeforeOperator.append(0);                //On click add 0
+                tvCalculate.setText(strBeforeOperator);
                 break;
 
             case R.id.btn1:
-                string += 1;
+
+                strBeforeOperator.append(1);
+                tvCalculate.setText(strBeforeOperator);   //On click add 1
                 break;
 
             case R.id.btn2:
-                string += 2;
+                strBeforeOperator.append(2);
+                tvCalculate.setText(strBeforeOperator);  //On click add 2
                 break;
 
             case R.id.btn3:
-                string += 3;
+                strBeforeOperator.append(3);
+                tvCalculate.setText(strBeforeOperator);  //On click add 3
                 break;
 
             case R.id.btn4:
-                string += 4;
+                strBeforeOperator.append(4);
+                tvCalculate.setText(strBeforeOperator);  //On click add 4
                 break;
 
             case R.id.btn5:
-                string += 5;
+                strBeforeOperator.append(5);
+                tvCalculate.setText(strBeforeOperator);  //On click add 5
                 break;
 
             case R.id.btn6:
-                string += 6;
+                strBeforeOperator.append(6);
+                tvCalculate.setText(strBeforeOperator);  //On click add 6
                 break;
 
             case R.id.btn7:
-                string += 7;
+                strBeforeOperator.append(7);
+                tvCalculate.setText(strBeforeOperator);  //On click add 7
                 break;
 
             case R.id.btn8:
-                string += 8;
+                strBeforeOperator.append(8);
+                tvCalculate.setText(strBeforeOperator);  //On click add 8
                 break;
 
             case R.id.btn9:
-                string += 9;
+                strBeforeOperator.append(9);
+                tvCalculate.setText(strBeforeOperator);  //On click add 9
+                break;
+
+            case R.id.btnClear:
+                strBeforeOperator.setLength(0);    //On click Clear
+                tvCalculate.setText("Calculate");
+                if (count == 1){
+                    strBeforeOperator.setLength(0);
+                    tvResult.setText("");
+                    tvCalculate.setText("");
+                }
+                count = 0;
+                break;
+
+            case R.id.btnDecimal:
+                strBeforeOperator.append(".");
+                tvCalculate.setText(strBeforeOperator);  // On Click Clear
                 break;
 
             case R.id.btnAdd:
-                addNumbers();
+                operator = "add";
+                strBeforeOperator.append("+");
+                tvCalculate.setText(strBeforeOperator);
+                if (count == 1) {
+                    strBeforeOperator.setLength(0);
+                    strBeforeOperator.append(tvResult.getText() + "+");
+                    tvCalculate.setText(strBeforeOperator);
+
+                }
                 break;
 
+            case R.id.btnMin:
+                operator = "min";
+                strBeforeOperator.append("-");
+                tvCalculate.setText(strBeforeOperator);
+                if (count == 1) {
+                    strBeforeOperator.setLength(0);
+                    strBeforeOperator.append(tvResult.getText() + "-");
+                    tvCalculate.setText(strBeforeOperator);
+                }
+                break;
 
+            case R.id.btnMul:
+                operator = "mul";
+                strBeforeOperator.append("*");
+                tvCalculate.setText(strBeforeOperator);
+
+                if (count == 1) {
+                    strBeforeOperator.setLength(0);
+                    strBeforeOperator.append(tvResult.getText() + "*");
+                    tvCalculate.setText(strBeforeOperator);
+                }
+                break;
+
+            case R.id.btnDiv:
+                operator = "div";
+                strBeforeOperator.append("/");
+                tvCalculate.setText(strBeforeOperator);
+
+                if (count == 1) {
+                    strBeforeOperator.setLength(0);
+                    strBeforeOperator.append(tvResult.getText() + "/");
+                    tvCalculate.setText(strBeforeOperator);
+                }
+                break;
+
+            case R.id.btnEnter:
+
+                count = 1;
+                if (operator.equals("add")) {        //
+                    equation = strBeforeOperator.toString().split("\\+", 2);
+                    num1 = equation[0];
+                    num2 = equation[1];
+                    String result = addNumbers(num1, num2);
+                    tvResult.setText(result);
+                    num1 = result;
+
+                } else if (operator.equals("min")) {
+                    equation = strBeforeOperator.toString().split("-", 2);
+
+                    num1 = equation[0];
+                    num2 = equation[1];
+                    String result = subNumbers(num1, num2);
+                    tvResult.setText(result);
+                    num1 = result;
+
+                } else if (operator.equals("mul")) {
+                    equation = strBeforeOperator.toString().split("\\*", 2);
+
+                    num1 = equation[0];
+                    num2 = equation[1];
+                    String result = mulNumbers(num1, num2);
+                    tvResult.setText(result);
+                    num1 = result;
+
+                } else if (operator.equals("div")) {
+                    equation = strBeforeOperator.toString().split("/", 2);
+
+                    num1 = equation[0];
+                    num2 = equation[1];
+                    String result = divNumbers(num1, num2);
+                    tvResult.setText(result);
+                    num1 = result;
+
+                }
 
         }
     }
